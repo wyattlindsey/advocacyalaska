8ce9e100f52c26e96fd769567e63567ba1448284
wyattlindsey <wyatt@spatially.com>
2018-01-30 11:26:21 -0800

    put serve-favicon first

--- dist/index.js
+++ dist/index.js
@@ -5,7 +5,7 @@ var favicon = require('serve-favicon');
 var path = require('path');
 var PORT = process.env.PORT || 5000;
 
-express().use(express.static(path.join(__dirname, 'dist'))).use(favicon(path.join(__dirname, 'dist', 'favicon.ico'))).get('/', function (req, res) {
+express().use(favicon(path.join(__dirname, 'dist', 'favicon.ico'))).use(express.static(path.join(__dirname, 'dist'))).get('/', function (req, res) {
   return res.render('dist');
 }).listen(PORT, function () {
   return console.log('Listening on ' + PORT);
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'dist')))
   .use(favicon(path.join(__dirname, 'dist', 'favicon.ico')))
+  .use(express.static(path.join(__dirname, 'dist')))
   .get('/', (req, res) => res.render('dist'))
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
